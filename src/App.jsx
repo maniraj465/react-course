5717efe854eb2f1ec1c53c9b490220773f4d2bd1
Maniraj Sivasubbu <38633966+maniraj465@users.noreply.github.com>
2026-06-11 16:12:07 +0530

    AI fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import NavBar from './components/navbar/NavBar'
-import Course from './components/course/Course'
 import Footer from './components/footer/Footer'
-import reactLogo from './assets/react.svg';
 import CourseList from './components/courselist/courseList'
 
 function App() {
-
   return (
-    <>
-      <NavBar/>
-      <CourseList/>
-      <Footer/>
-    </>
+    <div className="app">
+      <NavBar />
+      <main className="content">
+        <CourseList />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
